4852977b27d6e38c565019836e343a25f3383a17
Jorik Devreese <jorikdevreese@hotmail.be>
2024-02-12 12:00:31 +0100

    removed unused code

--- script_conversion/scripts/main.js
+++ script_conversion/scripts/main.js
@@ -37,40 +37,6 @@ class IconFactory {
         // return the add icon
         return addIcon;
     }
-
-    static getUploadIcon(tag) {
-        // create the upload icon
-        const uploadIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        uploadIcon.setAttribute('viewBox', '0 -960 960 960');
-        uploadIcon.setAttribute('preserveAspectRatio', 'xMidYMid meet')
-        uploadIcon.classList.add('icon-0', tag);
-        uploadIcon.innerHTML = `<path d="M452-202h60v-201l82 82 42-42-156-152-154 154 42 42 84-84v201ZM220-80q-24 0-42-18t-18-42v-680q0-24 18-42t42-18h361l219 219v521q0 24-18 42t-42 18H220Zm331-554v-186H220v680h520v-494H551ZM220-820v186-186 680-680Z"/></path>`;
-        // return the upload icon
-        return uploadIcon;
-    }
-
-    static getDownloadIcon(tag) {
-        // create the download icon
-        const downloadIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        downloadIcon.setAttribute('viewBox', '0 -960 960 960');
-        downloadIcon.setAttribute('preserveAspectRatio', 'xMidYMid meet')
-        downloadIcon.classList.add('button', tag);
-        downloadIcon.innerHTML = `<path d="M840-683v503q0 24-18 42t-42 18H180q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h503l157 157Zm-60 27L656-780H180v600h600v-476ZM479.765-245Q523-245 553.5-275.265q30.5-30.264 30.5-73.5Q584-392 553.735-422.5q-30.264-30.5-73.5-30.5Q437-453 406.5-422.735q-30.5 30.264-30.5 73.5Q376-306 406.265-275.5q30.264 30.5 73.5 30.5ZM233-584h358v-143H233v143Zm-53-72v476-600 124Z"/></path>`;
-        // return the download icon
-        return downloadIcon;
-    }
-
-    static getSearchIcon(tag) {
-        // create the download icon
-        const downloadIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        downloadIcon.setAttribute('viewBox', '0 0 24 24');
-        downloadIcon.setAttribute('preserveAspectRatio', 'xMidYMid meet')
-        downloadIcon.classList.add('button', tag);
-        downloadIcon.innerHTML = `<path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></path>`;
-        // return the download icon
-        return downloadIcon;
-    }
-
 }
 
 // VariableGroup: this class will be used to store variables
@@ -192,40 +158,12 @@ class VariableWrapper {
     variableWrapperElement = null;
     variableGroups = [];
     globalVariableGroup = null;
-    prevLanguage = null;
 
     // constructor
     constructor() {
         this.createVariableWrapper();
-        this.setDynamicLanguageConfig();
+        datalists["datalist-languages"].addDynamicEventListeners(document.querySelector('.language-config'));
     }
-
-    // function to set the dynamic language Config
-    setDynamicLanguageConfig() {
-        const languageConfig = document.querySelector('.language-config');
-
-        languageConfig.addEventListener('focus', (event) => {
-            this.prevLanguage = event.target.value
-            event.target.value = '';
-        });
-
-        languageConfig.addEventListener('blur', (event) => {
-            const language = event.target.value;
-            let validLanguageDataList = datalists["datalist-languages"].json;
-            if (!validLanguageDataList.includes(language)) {
-                event.target.value = this.prevLanguage;
-            }
-        });
-
-        // run blur upon pressing enter
-        languageConfig.addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') {
-                event.target.blur();
-            }
-        });
-    }
-            
-
 
     // function to create the variable wrapper
     createVariableWrapper() {
@@ -1041,10 +979,10 @@ class ScriptHandler {
     }
 
     // function to add the import and export buttons
-    addImportExportButtons() {
+    addEventListeners() {
         // select the top bar of the script area
-        const topBar = document.querySelector('.toolbar');
         let searchInput = document.querySelector('.script-search')
+        datalists["datalist-scripts"].addDynamicEventListeners(searchInput);
 
         searchInput.addEventListener('change', () => {
                 const scriptInfo = datalists['datalist-scripts'].getOptionByName(searchInput.value);
@@ -1166,5 +1104,5 @@ document.addEventListener("DOMContentLoaded", async function () {
     scriptHandler = new ScriptHandler();
     scriptHandler.addEventListeners();
     scriptHandler.addScriptElement(); // add a default first script element
-    scriptHandler.addImportExportButtons();
+    scriptHandler.addEventListeners();
 });
